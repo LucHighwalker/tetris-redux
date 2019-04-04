69bfdd8306e45135204971fdf16ef177250ea959
LucHighwalker <luc.highwalker@gmail.com>
2019-04-04 15:01:33 -0700

    - connected next block to redux

--- src/components/nextBlock/nextBlock.jsx
+++ src/components/nextBlock/nextBlock.jsx
@@ -1,41 +1,54 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import { connect } from "react-redux";
 
-import GridSquare from '../gridSquare/gridSquare'
+import GridSquare from "../gridSquare/gridSquare";
 
-import './nextBlock.scss';
+import { shapes } from '../../utils'
+
+import "./nextBlock.scss";
 
 class NextBlock extends Component {
-	constructor(props) {
-		super(props);
-    
-		// states go here.
-	}
+  constructor(props) {
+    super(props);
 
-	makeGrid() {
-    const box = [[0,0,0,0], [0,0,0,0], [0,0,0,0], [0,0,0,0]]
+    // states go here.
+  }
+
+  makeGrid() {
+    // deconstruct shape
+    const { shape } = this.props;
+    // get the array for this shape first rotation
+    const block = shapes[shape][0]
+    // get the empty shape
+    const box = shapes[0][0]        
+  
     // Map the block to the grid
     return box.map((rowArray, row) => {
       return rowArray.map((square, col) => {
-        return <GridSquare key={`${row}${col}`} color={0} />
-      })})
-    }
+        // If there is a 1 use the shape index
+        const color = block[row][col] === 0 ? 0 : shape
+        return <GridSquare key={`${row}${col}`} color={color} />
+      })
+    })
+  }
 
-  render () {
-    return (
-      <div className="next-block">
-        {this.makeGrid()}
-      </div>
-    )
+  render() {
+    return <div className="next-block">{this.makeGrid()}</div>;
   }
 }
 
-const mapStateToProps = state => ({
-	...state
-});
+const mapStateToProps = state => {
+  // Return nextShape as shape
+  return {
+    shape: state.gameReducer.nextShape
+  }
+};
 
 const mapDispatchToProps = dispatch => ({
-	// action: () => dispatch(action())
+  // action: () => dispatch(action())
 });
 
-export default connect(mapStateToProps, mapDispatchToProps)(NextBlock);
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(NextBlock);
