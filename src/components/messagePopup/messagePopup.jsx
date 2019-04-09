42155fe87bab67d0b263705828fd4a040997a549
LucHighwalker <luc.highwalker@gmail.com>
2019-04-09 10:31:10 -0700

    connected message popup

--- src/components/messagePopup/messagePopup.jsx
+++ src/components/messagePopup/messagePopup.jsx
@@ -1,30 +1,51 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import { connect } from "react-redux";
-import './messagePopup.scss';
+import "./messagePopup.scss";
 
 class MessagePopup extends Component {
-	constructor(props) {
-		super(props);
-    
-		// states go here.
-	}
+  constructor(props) {
+    super(props);
 
-	render() {
+    // states go here.
+  }
+
+  render() {
+    const { gameOver, isRunning } = this.props
+    let message = ''
+    let isHidden = 'hidden'
+
+    // If the game is over, show the popup saying "Game Over"
+    if (gameOver) {
+      message = 'Game Over'
+      isHidden = ''
+    // If the game isn't running, it must be paused, so show the popup saying "Paused"
+    } else if (!isRunning) {
+      message = 'Paused'
+      isHidden = ''
+    // Default message, will still be hidden
+    } else {
+      message = '???'
+    }
+
     return (
-      <div className='message-popup'>
-        <h1>Message Title</h1>
-        <p>Message info...</p>
+      <div className={`message-popup ${isHidden}`}>
+        <h1>{message}</h1>
+        <p></p>
       </div>
     )
   }
 }
 
 const mapStateToProps = state => ({
-	...state
+  gameOver: state.game.gameOver,
+  isRunning: state.game.isRunning
 });
 
 const mapDispatchToProps = dispatch => ({
-	// action: () => dispatch(action())
+  // action: () => dispatch(action())
 });
 
-export default connect(mapStateToProps, mapDispatchToProps)(MessagePopup);
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(MessagePopup);
