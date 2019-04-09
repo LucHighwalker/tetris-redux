b118642841c280b7d947afe1fe656d23e814bd7d
LucHighwalker <luc.highwalker@gmail.com>
2019-04-09 10:28:15 -0700

    connected controls to redux

--- src/components/controls/controls.jsx
+++ src/components/controls/controls.jsx
@@ -1,48 +1,62 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import { connect } from "react-redux";
-import './controls.scss';
+import "./controls.scss";
+
+import { moveDown, moveLeft, moveRight, rotate } from "../../redux/actions";
 
 class Controls extends Component {
-	constructor(props) {
-		super(props);
-    
-		// states go here.
-	}
+  constructor(props) {
+    super(props);
 
-	render() {
-		return (
-			<div className="controls">
+    // states go here.
+  }
+
+  render() {
+    const { isRunning } = this.props
+
+    return (
+      <div className="controls">
         {/* left */}
         <button className="control-button" onClick={(e) => {
-
+          if (!isRunning) { return }
+          this.props.moveLeft()
         }}>Left</button>
 
         {/* right */}
         <button className="control-button" onClick={(e) => {
-
+          if (!isRunning) { return }
+          this.props.moveRight()
         }}>Right</button>
 
         {/* rotate */}
         <button className="control-button" onClick={(e) => {
-
+          if (!isRunning) { return }
+          this.props.rotate()
         }}>Rotate</button>
 
         {/* down */}
         <button className="control-button" onClick={(e) => {
-
+          if (!isRunning) { return }
+          this.props.moveDown()
         }}>Down</button>
 
       </div>
-		);
-	}
+    )
+  }
 }
 
 const mapStateToProps = state => ({
-	...state
+  isRunning: state.game.isRunning
 });
 
 const mapDispatchToProps = dispatch => ({
-	// action: () => dispatch(action())
+  moveRight,
+  moveLeft,
+  moveDown,
+  rotate
 });
 
-export default connect(mapStateToProps, mapDispatchToProps)(Controls);
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Controls);
